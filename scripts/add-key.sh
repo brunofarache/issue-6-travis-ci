fbfc44b55572e927adc553a04e8b4f1ff466a3d9
Bruno Farache <bruno.farache@liferay.com>
2017-03-04 07:14:31 -0300

    Changes needed by macOS Sierra, otherwise build gets stuck
    See:
    http://stackoverflow.com/questions/40994404/xcodebuild-stuck-at-codesign-step-when-being-run-in-travis-ci/41013151#41013151
    https://github.com/travis-ci/travis-ci/issues/6791#issuecomment-261215038

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 security create-keychain -p travis ios-build.keychain
-security import ./scripts/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./scripts/certs/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+security import ./scripts/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -A
+security import ./scripts/certs/dist.cer -k ~/Library/Keychains/ios-build.keychain -A
+security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -A
+security set-key-partition-list -S apple-tool:,apple: -s -k travis ios-build.keychain
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./scripts/profile/$PROFILE_NAME.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
